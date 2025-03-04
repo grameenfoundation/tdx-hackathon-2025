5dbaca030fd5cfe3acb3cd64eb68d039e452c22d
Louise Racette <louise@taroworks.org>
2025-03-03 23:07:16 -0800

    Save button in LWC kinda works

--- mock-data/force-app/main/default/lwc/generateJavascriptSuggestion/generateJavascriptSuggestion.js
+++ mock-data/force-app/main/default/lwc/generateJavascriptSuggestion/generateJavascriptSuggestion.js
@@ -1,8 +1,15 @@
 import { api, LightningElement } from "lwc";
 import SuggestJavascript from "@salesforce/apex/GenerateJavascriptSuggestion.SuggestJavascript";
+import { updateRecord } from "lightning/uiRecordApi";
+import { generateRecordInputForUpdate } from 'lightning/uiRecordApi';
+
+
+import ID_FIELD from "@salesforce/schema/Question__c.Id";
+import JS_FIELD from "@salesforce/schema/Question__c.DynamicOperation__c";
+
 
 export default class GenerateJavascriptSuggestion extends LightningElement {
-  generatedJS;
+  currentJS;
   error;
   showSpinner = false;
   @api recordId;
@@ -13,13 +20,38 @@ export default class GenerateJavascriptSuggestion extends LightningElement {
       const suggestion = await SuggestJavascript({
         questionID: this.recordId
       });
-       this.generatedJS = suggestion;
+       this.currentJS = suggestion;
       this.error = undefined;
     } catch (error) {
-      this.generatedJS = undefined;
+      this.currentJS = undefined;
       this.error = error;
     } finally {
       this.showSpinner = false;
     }
   }
+
+  handleChange(event) {
+    this.currentJS = event.target.value;
+}
+  
+  async handleClick() {
+        const fields = {};
+
+        fields[JS_FIELD.fieldApiName] = this.currentJS;
+        fields[ID_FIELD.fieldApiName] = this.recordId;
+            
+        const recordInput = {
+            fields: fields
+        };
+        console.log(fields);
+        console.log(recordInput);
+
+        try {
+            const record = await updateRecord(recordInput);
+            console.log(record);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+  
 }
